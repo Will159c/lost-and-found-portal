77e31654970f3d13a4018d95584998e0ecb42763
Will159c <163231055+Will159c@users.noreply.github.com>
2025-09-15 17:50:46 -0700

    initial commit with next.js, react and typescript

---
+++ src/app/(site)/home/page.tsx
@@ -0,0 +1,5 @@
+import HomeContainer from "../../components/feature/home/home-container";
+
+export default function HomePage() {
+	return (<HomeContainer/>)
+}
---
+++ src/app/components/feature/home/home-container.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export default function HomeContainer() {
+  return (
+    <div>Hello Home</div>
+  );
+}
